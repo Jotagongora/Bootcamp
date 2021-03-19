611e14ffd90e784634f205fcdd221c153b55b27a
Juan Alberto <gongorarealjota@gmail.com>
2021-03-19 13:52:34 +0100

    Javascript 2ªclase

--- Javascript/Cheatsheet/js/main.js
+++ Javascript/Cheatsheet/js/main.js
@@ -61,8 +61,9 @@ const x = 1;
 console.clear();
 
 // ---------------------Tipos de Datos--------------
+//---------------Datos primitivos-------------------
 let str = String("I'm a string");
-let str2 = "I'm another string"; 
+let str2 = "I'm another string"; // Recomendada
 console.log(typeof str, typeof str2, typeof "");
 
 // let str = "string" ES IGUAL A "string"
@@ -70,4 +71,105 @@ console.log(typeof str, typeof str2, typeof "");
 let num = Number(5);
 let num2 = 5;
 console.log(typeof num, typeof num2, typeof 5, typeof "5");
+
+
+let bool = Boolean(true);
+let bool2 = false;
+console.log(typeof bool, typeof bool2, typeof true, typeof "true");
+
+let undef;
+console.log(undef, typeof undef);
+
+let selectedColor = null;// Limpiar una variable
+console.log(selectedColor, typeof selectedColor);
+
+// Objects
+
+array = Array(1, 2, 3);
+array2 = [1, 2, 3];
+console.log(array, array2);
+console.log(typeof array, typeof array2);
+
+// let coche = {
+//     nPuertas: 5,        Spoiler crear objetos
+//     color: "red",
+//     turnOn() {
+//         //funcionalidad
+//     }
+// }
+
+console.log(typeof new Date("04-03-1987"));
+console.log(typeof new Error("Mensaje de error"));
+
+console.clear();
+// --------------------- String ------------------------
+str = "Hola futuro";
+console.log(str, str.length);
+console.log(str.indexOf("futuro"));
+console.log(str.lastIndexOf("u"));
+console.log(str.substring(1, 3));
+
+console.log("Hola mundo".concat(". Adiós mundo"));
+console.log("Hola mundo" + ". Adiós mundo"); // Equivalente a concatenar
+
+str = str + ". Adiós Mundo";
+str += ". futuro Mundo";
+
+console.clear();
+
+console.log(str);
+console.log(str.replace("futuro", "Mundo"));
+console.log(str.replaceAll("futuro", "Mundo"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+console.log(`El día de mi cumpleaños es ${day}.`); // String template
+console.log("El día de mi cumpleaños es" , day, ".");
+
+// Parsing / (Parseo en castellanizado)
+let number = Number("asasas"); // NaN Not a Number. No genera error.
+console.log(number, typeof number);
+
+number = Number("1000");
+console.log(number, typeof number);
+
+number += "1000";
+console.log(number, typeof number);
+
+console.log("1" + 1, "1" - 1);
+console.clear();
+//------------ Objects ---------------------
+
+let person = { // Curly brackets
+    name: "Jhon",
+    age: 30,
+    blonde: false,
+    walk: function () {
+        console.log("Estoy andando")
+    }, // Pre-ES6
+    jump() {
+        console.log("Hey estoy saltando");
+    }
+}
+
+// Acceso por punto / Dot notation
+console.log(person);
+console.log(`Se llama ${person.name} y tiene ${person.age} años`);
+person.name = "Jane";
+console.log(person);
+console.log(`Se llama ${person.name} y tiene ${person.age} años`);
+// Acceso por corchetes / Brackets notation
+
+let propertyName = "age";
+console.log(person[propertyName]);
+
+// Ampliación
+
+person.children = ["Martha", "Peter"];
+console.log(person);
+
+person.jump();
+person.walk();
+
+
 
