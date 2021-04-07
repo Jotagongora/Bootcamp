07322d01f9811d578f3af37d3f652b7db0d73d13
JotaCasa <gongorarealjota@gmail.com>
2021-04-08 00:38:17 +0200

    intento de ejercicio 2

--- Javascript/Ejercicios/Ejercicio2/js/main.js
+++ Javascript/Ejercicios/Ejercicio2/js/main.js
@@ -34,4 +34,18 @@ users.forEach(user => {
     select.appendChild(option);
 });
 
+/* Rellenar lista con datos de un usuario cualquiera*/
+
+
+
+for (const property in users[0]) {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.textContent = `${property}: ${users[0][property]}`;
+    list.appendChild(li);
+};
+
+
+
+
 
