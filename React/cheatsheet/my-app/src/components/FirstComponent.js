ca0b18a51e490fe56370875fb01e021a3c55ad4d
Juan Alberto <gongorarealjota@gmail.com>
2021-04-19 13:56:41 +0200

    React comienzo

---
+++ React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex culpa eum animi ratione neque, voluptatum voluptatem repellendus minus odit tempora itaque, ad possimus numquam. Facere tempora quos esse sapiente illum.</p>
+        </div>
+    )
+}
+
+// Export default function. alternativa//
+
