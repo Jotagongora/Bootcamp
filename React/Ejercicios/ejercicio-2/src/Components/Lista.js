a60aec884cb30358366d9d24c4e38e2b3310b620
Juan Alberto <gongorarealjota@gmail.com>
2021-04-21 10:15:32 +0200

    Ejercicio 2 entero

--- React/Ejercicios/ejercicio-2/src/Components/Lista.js
+++ React/Ejercicios/ejercicio-2/src/Components/Lista.js
@@ -1,18 +1,17 @@
 
 
-function Lista(props) {
+export default function Lista(props) {
+    const {categoria, productos} = props;
     return (
-        props.producto.map(product => {
-            return (
-                <div>
-                    <h3>{props.categoria}</h3>
-                    <ul>
-                        <li>{product.producto}</li>
-                    </ul>
-                </div>
-            )
-        })
+        <div>
+            <h3>{categoria}</h3>
+            <ul>{productos.map(({id, nombre, marca, modelo, precio}) => {
+                return (<li key={id}>
+                            {nombre} {marca} {modelo}.Precio: {precio}
+                        </li>)
+                })}
+            </ul>
+        </div>      
     )
 }
 
-export {Lista};
--- React/Ejercicios/ejercicio-2/src/Components/Tienda.js
+++ React/Ejercicios/ejercicio-2/src/Components/Tienda.js
@@ -1,28 +1,28 @@
-import {Lista} from './Lista';
+import Lista from './Lista';
 
 function Tienda() {
     const tienda = {
         electronica: [
-            {id: 27, producto: "Televisor", marca: "LG", modelo: "XP7302", precio: 399},
-            {id: 28, producto: "Equipo Hi-FI", marca: "Samsumg", modelo: "VF235", precio: 179},
-            {id: 29, producto: "Televisor", marca: "Sony", modelo: "Bravia-173", precio: 498},
+            {id: 27, nombre: "Televisor", marca: "LG", modelo: "XP7302", precio: 399},
+            {id: 28, nombre: "Equipo Hi-FI", marca: "Samsumg", modelo: "VF235", precio: 179},
+            {id: 29, nombre: "Televisor", marca: "Sony", modelo: "Bravia-173", precio: 498},
         ],
         alimentacion: [
-            {id: 30, producto: "Galletas", marca: "Maria", precio: 0.90},
-            {id: 31, producto: "Ensalada", marca: "Imizurra", precio: 1.30},
-            {id: 32, producto: "Patatas", marca: "McKain", precio: 0.90},
-            {id: 33, producto: "Pasta", marca: "Gallo", precio: 0.90},
+            {id: 30, nombre: "Galletas", marca: "Maria", precio: 0.90},
+            {id: 31, nombre: "Ensalada", marca: "Imizurra", precio: 1.30},
+            {id: 32, nombre: "Patatas", marca: "McKain", precio: 0.90},
+            {id: 33, nombre: "Pasta", marca: "Gallo", precio: 0.90},
         ],
         mascotas: [
-            {id: 34, producto: "Corquetas para gato", marca: "Purina", precio: 4.90},
-            {id: 35, producto: "arena de gato", marca: "Limpior", precio: 1.10},
+            {id: 34, nombre: "Corquetas para gato", marca: "Purina", precio: 4.90},
+            {id: 35, nombre: "arena de gato", marca: "Limpior", precio: 1.10},
         ]
     }
     return (
         <div>
-            <Lista categoria="electronica" producto={tienda.producto}/>
-            <Lista categoria="alimentacion" producto={tienda.producto}/>
-            <Lista cateogira="mascotas" producto={tienda.producto}/>
+            <Lista categoria="Electrónica" productos={tienda.electronica}/>
+            <Lista categoria="Alimentacion" productos={tienda.alimentacion}/>
+            <Lista categoria="Mascotas" productos={tienda.mascotas}/>
         </div>
     )
 }
