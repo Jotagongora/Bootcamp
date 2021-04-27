bcf3b5e1c81f3c5856ff0f23f9207dc0cfdbe3e0
Jota-casa <gongorarealjota@gmail.com>
2021-04-27 23:50:23 +0200

    ejercicio 5 semiterminado

--- React/Ejercicios/ejercicio-5/src/App.js
+++ React/Ejercicios/ejercicio-5/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Components/Navbar.css';
 import Router from './Router';
 
 
--- React/Ejercicios/ejercicio-5/src/Router.js
+++ React/Ejercicios/ejercicio-5/src/Router.js
@@ -4,14 +4,26 @@ import Page1 from './Pages/Page1';
 import Page2 from './Pages/Page2';
 import Page3 from './Pages/Page3';
 import Page4 from './Pages/Page4';
+import './Components/Navbar.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Router() {
     return (
         <div>
-            <Page1 />
-            <Page2 />
-            <Page3 />
-            <Page4 />
+            <BrowserRouter>
+                <nav className="navbar bg-dark navbar-dark">
+                <NavLink exact to="/Page1">Ejercicio 1</NavLink>
+                <NavLink exact to="/Page2">Ejercicio 2</NavLink>
+                <NavLink exact to="/Page3">Ejercicio 3</NavLink>
+                <NavLink exact to="/Page4">Ejercicio 4</NavLink>
+                </nav>
+                <Switch>
+                    <Route exact path="/Page1" component={Page1} />
+                    <Route exact path="/Page2" component={Page2} />
+                    <Route exact path="/Page3" component={Page3} />
+                    <Route exact path="/Page4" component={Page4} />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
