e2a835752573b12d9c52dfd311d542198dca62fd
Juan Alberto <gongorarealjota@gmail.com>
2021-05-17 13:54:20 +0200

    Toque final chuleta

--- NodeJs/cheatsheet/server.js
+++ NodeJs/cheatsheet/server.js
@@ -6,14 +6,10 @@ const express = require("express");
 
 const app = express();
 
-const users = require("./routes/users");
-const login = require("./routes/login");
-
 //Antes de los endpoints los middlewares
 app.use(express.json());
 
-app.use("/users", users);
-app.use("/login", login);
+app.use(require("./routes/index"));
 
 mongoose.connect("mongodb://localhost:27017/users", {
     useNewUrlParser: true,
