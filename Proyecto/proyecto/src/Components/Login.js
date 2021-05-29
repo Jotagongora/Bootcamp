6e7097881cdc7e1204ca9d12e29c90dc8a538f0c
Jota Amd <gongorarealjota@gmail.com>
2021-05-29 19:56:29 +0200

    Proyecto Final

---
+++ Proyecto/proyecto/src/Components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.css';
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="loginImg"></div>
+            <div>
+                <h3>Iniciar sesión</h3>
+                <form action="">
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
